777fddb26d3d13e9dd95c96bf8bf7273f8ddb8ec
Alejandro Becerra <alejobecerra1995@gmail.com>
2020-11-23 21:25:49 -0500

    tried with string methods but was not able to correctly use them. Will review and re attempt

---
+++ fundamentos_fullstack_js/strings/ejercicios_strings.js
@@ -0,0 +1,51 @@
+//console.log("Hola mundo"[3]);
+
+//console.log("    "[2]);
+
+//console.log("El azul es mi color favorito"[12]);
+
+//console.log(""[0]);
+
+//console.log(" El cielo es azul"[20]);
+
+//count = 0
+
+//function countingEs(string){
+//var i = 0;
+//	while (i < string.length){
+//	if (string [i] === 'e'){
+//	count = count + 1}
+//		}
+//	
+//return count;
+//}
+//console.log(countingEs("Estoy ejercitándome los miércoles y jueves") === 6);  // true
+//console.log(countingEs("Eeeeh!, que alegría verlos amigos mios")  === 6); // true
+//console.log(countingEs("Ejemplares son las personas de este hermoso pueblo")  === 8); // true
+//console.log(countingEs("Ahora qué?") === 0); // true
+//console.log(countingEs("LOs elefantes son enormes") === 5) // true	
+
+//este de arriba la verdad me costo algo de  trabajo entender como atacar el problema
+//creo que tengo que buscar problemas asi y volver a intentarlo. 
+
+//function generatePassword(string){
+//	var i = 0
+//	string.toLowerCase();
+//	while (i < string.length){
+//		if (string[i] === "a"){
+//			string[i]="4"}
+//		else if (string[i] === "e"){
+//                      string[i]="3"}
+//              else if  (string[i] === "i"){
+//                        string[i]="1"}
+//		else  if (string[i] === "o"){
+//                      string[i]="0"}
+//		}
+//	i = i + 1;
+//	//string.join(",") = password;
+//return  string;
+//console.log(string);
+//	}
+///	console.log(generatePassword("hola"));
+
+//voy a volver a revisar la parte de string methods y ma;ana pido ayuda en la llamada con german para ver si entiendo bien este tema.  
