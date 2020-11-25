eaf54badaf9bf4fcf3d804104f9183a33af8138a
Alejandro <alejobecerra1995@gmail.com>
2020-11-24 20:47:14 -0500

    added second and third part of the recipe function

--- fundamentos_fullstack_js/objetos/objects.js
+++ fundamentos_fullstack_js/objetos/objects.js
@@ -12,14 +12,15 @@
 //  console.log("  Categorías: " + product.categories.join(", "));
 //}
 
-// primera ejercicio de receta maestra
+// primer ejercicio de receta maestra
 // 
 
-
-var ingredientes = {
+igs: var ingredientes = {
 ingrediente1: {id: 1, name:"leche en polvo", medida1: "1 litro"},
 ingrediente2: {id: 2, name :"chocolate en barra", medida2: "3 barras"},
-ingrediente3: {id: 3, name: "azucar", medida3: "3 gramos"}}
+ingrediente3: {id: 3, name: "azucar", medida3: "3 gramos"},
+mixIngredientes: function mix(){ (console.log ("Mezclando los ingredientes"))}
+}
 function receta(){
     console.log("Necesitas los siguientes ingredientes: ")
     console.log(ingredientes.ingrediente1.id + " " + ingredientes.ingrediente1.name + ", " + ingredientes.ingrediente1.medida1);
@@ -27,4 +28,16 @@ function receta(){
     console.log(ingredientes.ingrediente3.id + " " + ingredientes.ingrediente3.name + ", " + ingredientes.ingrediente2.medida3);
 }
 
-console.log(receta())
+function orden_receta(){
+    var paso1  = ("Primero agrega " + ingredientes.ingrediente1.medida1 + " de " + ingredientes.ingrediente1.name);
+    var paso2 = ("Después agrega " + ingredientes.ingrediente2.medida2 + " de " + ingredientes.ingrediente2.name);
+    var paso3 = ("Por último agrega " + ingredientes.ingrediente3.medida3 + " de " + ingredientes.ingrediente3.name);
+    console.log(paso1);
+    console.log(paso2);
+    console.log(paso3);
+}
+
+
+console.log(receta());
+console.log(orden_receta());
+console.log(ingredientes.mixIngredientes());
